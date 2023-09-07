66696e51b99c7648ef602d3d962627d5c7bcb14a
Ashok Koda <a.koda59@gmail.com>
2023-09-07 19:33:54 +0530

    Role entity added.

---
+++ src/main/java/com/example/hrstop/Entity/Role.java
@@ -0,0 +1,27 @@
+package com.example.hrstop.Entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "role_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int role_id;
+    @Column(name = "role_name")
+    private String role_name;
+}
