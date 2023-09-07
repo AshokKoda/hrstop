ad150da8a2f9a9893c97d3910fe63a56d5f9eff1
Ashok Koda <a.koda59@gmail.com>
2023-09-07 21:15:00 +0530

    Role is added in employee entity.

--- src/main/java/com/example/hrstop/Entity/Employee.java
+++ src/main/java/com/example/hrstop/Entity/Employee.java
@@ -1,24 +1,32 @@
 package com.example.hrstop.Entity;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "emp_table")
+@Table(name = "emp_table", uniqueConstraints = { @UniqueConstraint(columnNames = "emp_name"),
+        @UniqueConstraint(columnNames = "emp_email") })
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "emp_id")
@@ -35,4 +43,8 @@ public class Employee {
     private String emp_phoneno;
     @Column(name = "emp_doj")
     private Date emp_doj;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "emp_roles", joinColumns = @JoinColumn(name = "emp_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
