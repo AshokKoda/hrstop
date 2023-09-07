3e8c00326d315d8e6e831626b1e121bb5a73264a
Ashok Koda <a.koda59@gmail.com>
2023-09-07 21:30:27 +0530

    Add few lines in EmpRepository.

--- src/main/java/com/example/hrstop/Repository/EmpRepository.java
+++ src/main/java/com/example/hrstop/Repository/EmpRepository.java
@@ -1,9 +1,16 @@
 package com.example.hrstop.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hrstop.Entity.Employee;
 
 public interface EmpRepository extends JpaRepository<Employee, Integer> {
     
+    Optional<Employee> findByUsername(String emp_name);
+
+    Boolean existsByUsername(String emp_name);
+
+    Boolean existsByEmail(String emp_email);
 }
